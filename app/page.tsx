6ae0a9952b89d373421e1f3c3d74e42e66110f70
CuriousShivam <shivam.sah2023t@gmail.com>
2025-09-11 21:42:32 +0530

    Carousel Component Starting

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import { Link } from "@heroui/link";
-import { Snippet } from "@heroui/snippet";
-import { Code } from "@heroui/code";
-import { button as buttonStyles } from "@heroui/theme";
+// import { Link } from "@heroui/link";
+// import { Snippet } from "@heroui/snippet";
+// import { Code } from "@heroui/code";
+// import { button as buttonStyles } from "@heroui/theme";
+//
+// import { siteConfig } from "@/config/site";
+// import { title, subtitle } from "@/components/primitives";
+// import { GithubIcon } from "@/components/icons";
 
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import Carousel from "@/components/carousel";
 
 export default function Home() {
   return (
@@ -52,6 +54,6 @@ export default function Home() {
     //     </Snippet>
     //   </div>
     // </section>
-      <h1>Hello WOrld</h1>
+      <Carousel/>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -22,30 +22,9 @@ import {
 } from "@/components/icons";
 
 export const Navbar = () => {
-  // const searchInput = (
-  //   <Input
-  //     aria-label="Search"
-  //     classNames={{
-  //       inputWrapper: "bg-default-100",
-  //       input: "text-sm",
-  //     }}
-  //     endContent={
-  //       <Kbd className="hidden lg:inline-block" keys={["command"]}>
-  //         K
-  //       </Kbd>
-  //     }
-  //     labelPlacement="outside"
-  //     placeholder="Search..."
-  //     startContent={
-  //       <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-  //     }
-  //     type="search"
-  //   />
-  // );
-
   return (
-    <HeroUINavbar maxWidth="xl" position="sticky" >
-      <NavbarContent className="basis-1/5 sm:basis-full  " justify="start">
+    <HeroUINavbar className='' maxWidth="xl" position="sticky" >
+      <NavbarContent className="basis-1/5 sm:basis-full " justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
             <Logo />
@@ -58,9 +37,10 @@ export const Navbar = () => {
               <NextLink
                 className={clsx(
                   linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium ",
-                    "focus:outline-blue-800 focus-visible:ring-2 focus-visible:ring-primary rounded-md"
+                  "data-[active=true]:text-2xl data-[active=true]:font-medium",
+                    "rounded-md outline-none   focus-visible:outline-2 focus-visible:outline-blue-800 focus-visible:ring-2 focus-visible:ring-primary"
                 )}
+
                 color="foreground"
                 href={item.href}
               >
